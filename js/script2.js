1f000e594b746d2cb26804a02c14432ee12fb92a
Gioelinz <gioelerunza.gr@gmail.com>
2022-01-14 15:36:05 +0100

    add excercise 2 elements

---
+++ js/script2.js
@@ -0,0 +1,19 @@
+/* Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto */
+
+
+//target elementi dom
+
+const numberElement = document.getElementById("user-number");
+const cpuElement = document.getElementById("cpu-element");
+const resultElement = document.getElementById("result-element");
+
+//Funzione numero random
+
+function cpuNum() {
+
+}
